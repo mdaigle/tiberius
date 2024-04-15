57dd99f67d30208adebf60ed1e5b9f0d02f16eb7
Malcolm Daigle <mdaigle@microsoft.com>
2024-04-15 14:07:41 -0700

    Add back examples using tokio and async-std

---
+++ examples/tokio.rs
@@ -0,0 +1,30 @@
+use once_cell::sync::Lazy;
+use std::env;
+use tiberius::{Client, Config};
+use tokio::net::TcpStream;
+use tokio_util::compat::TokioAsyncWriteCompatExt;
+
+static CONN_STR: Lazy<String> = Lazy::new(|| {
+    env::var("TIBERIUS_TEST_CONNECTION_STRING").unwrap_or_else(|_| {
+        "server=tcp:localhost,51210;IntegratedSecurity=true;TrustServerCertificate=true;database=tiberius".to_owned()
+    })
+});
+
+#[cfg(not(all(windows, feature = "sql-browser-tokio")))]
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let config = Config::from_ado_string(&CONN_STR)?;
+
+    let tcp = TcpStream::connect(config.get_addr()).await?;
+    tcp.set_nodelay(true)?;
+
+    let mut client = Client::connect(config, tcp.compat_write()).await?;
+
+    let stream = client.query("SELECT * from people", &[]).await?;
+    let row = stream.into_row().await?.unwrap();
+
+    println!("{:?}", row);
+    assert_eq!(Some("Malcolm"), row.get(0));
+
+    Ok(())
+}
